3dc28a33cc193bfa9669ceb614b05b77e18c02fd
Carl <casteeleu@gmail.com>
2022-12-25 19:08:07 -0500

    debugging models.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner, Choice, Question
+from .models import *
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 class QuestionInline(admin.StackedInline):
     model = Question
-    extra=2
+    extra = 5
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra=2
+    extra = 5
 
 
 class LessonInline(admin.StackedInline):
@@ -29,17 +29,16 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
+# <HINT> Register Question and Choice models here
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ['question_content']
+    list_display = ['content']
 
-# <HINT> Register Question and Choice models here
 
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Submission)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-
-
